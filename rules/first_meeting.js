19f4f1fa4b95a5f0bba8ad1da9e01116ef664396
Jacob Swartwood <jswartwood@gmail.com>
2012-08-13 21:16:21 -0400

    Revising first meeting rule. Fixing various issues with it.

--- rules/first_meeting.js
+++ rules/first_meeting.js
@@ -14,7 +14,7 @@ var wakeRange = {
 
 console.log('Started script: Alert me of my first early morning meeting');
 
-var lastDateScreenUnlocked = Date.now()
+var lastDateScreenUnlocked = 0
   , screenUnlockOffset = 12 * 60 * 60 * 1000 // Snooze 12hrs
 ;
 
@@ -22,27 +22,37 @@ var lastDateScreenUnlocked = Date.now()
 device.screen.on('unlock', function() {
   var thisMoment = Date.now();
 
-  if (thisMoment < lastDateScreenUnlocked + screenUnlockOffset) return;
-  lastDateScreenUnlocked = thisMoment;
+  // Don't alert if we have already alerted today
+  if (thisMoment > lastDateScreenUnlocked + screenUnlockOffset) {
+    lastDateScreenUnlocked = thisMoment;
 
-  var day = new Date().toDateString()
-    , wakeStart = new Date(day + ' ' + wakeRange.start)
-    , wakeEnd = new Date(day + ' ' + wakeRange.end)
-    , calStart = new Date(day + ' ' + calendarRange.start)
-    , calEnd = new Date(day + ' ' + calendarRange.end)
-  ;
+    var day = new Date().toDateString()
+      , wakeStart = new Date(day + ' ' + wakeRange.start).getTime()
+      , wakeEnd = new Date(day + ' ' + wakeRange.end).getTime()
+      , calStart = new Date(day + ' ' + calendarRange.start).getTime()
+      , calEnd = new Date(day + ' ' + calendarRange.end).getTime()
+    ;
 
-  // check if the time permits to set the ringer mode
-  if (thisMoment >= wakeStart && thisMoment <= wakeEnd) {
-    // query for first appointment
-    var firstAppt = device.calendar.today[0];
-
-    if (firstAppt) {
-      var apptStart = new Date(firstAppt.startTime)
-        , apptTimeStr = apptStart.toTimeString().substring(0, 5)
+    // check if the time permits to set the ringer mode
+    if (thisMoment >= wakeStart && thisMoment <= wakeEnd) {
+      // query for first appointment
+      var firstAppt = device.calendar.today[0]
+        , apptIndex = 1
       ;
 
-      if (apptStart >= calStart && apptStart <= calEnd) {
+      // Find the first appointment in the calendar range
+      while (firstAppt) {
+        if (firstAppt.startTime >= calStart && firstAppt.startTime <= calEnd) {
+          break;
+        }
+        firstAppt = device.calendar.today[apptIndex++];
+      }
+
+      if (firstAppt) {
+        var apptStart = new Date(firstAppt.startTime)
+          , apptTimeStr = apptStart.toTimeString().substring(0, 5)
+        ;
+
         console.log('Alerting about ' + firstAppt.title + ' at ' + apptTimeStr);
 
         var notification = device.notifications.createNotification(firstAppt.title);
